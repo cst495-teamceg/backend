f1bd644c1a155d7cbe20f9e5d8c622a241032851
cameron babcock <cameron@devsofthewest.com>
2019-05-08 02:33:02 -0700

    fsa

--- src/main/java/com/devsofthewest/iphone/repositories/ArticleRepository.java
+++ src/main/java/com/devsofthewest/iphone/repositories/ArticleRepository.java
@@ -1,7 +1,7 @@
 package com.devsofthewest.iphone.repositories;
 
 import java.util.List;
-
+import com.google.appengine.api.datastore.Text;
 import org.springframework.cloud.gcp.data.datastore.repository.DatastoreRepository;
 
 import com.devsofthewest.iphone.model.Article;
